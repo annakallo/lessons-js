3787afcde0a676686b5b8766f34b64223e198a11
Anna <anna.kallo@gmail.com>
2020-05-22 18:42:21 +0300

    Lesson 4

--- 4-data-structures-objects-arrays.js
+++ 4-data-structures-objects-arrays.js
@@ -146,7 +146,263 @@ The lycanthrope's log:
 						name in brace notation isn’t followed by a value, its value is taken from 
 						the binding with the same name.
 
-						65old
+						-Arrays have an includes method that checks whether a given value exists in
+						the array. 
+
+
+						for (let i = 0; i < JOURNAL.length; i++) {
+						let entry = JOURNAL[i];
+						// Do something with entry
+						}
+						
+						- You can iterate over arrays using a special kind of for loop—for 
+						(let element of array)
+
+						for (let entry of JOURNAL) {
+						console.log(`${entry.events.length} events.`);
+						}
+
+						- When a for loop looks like this, with the word of after a variable definition,
+						it will loop over the elements of the value given after of. This works not only
+						for arrays but also for strings and some other data structures.
+
+Further arrayology: 	- push and pop - add and remove elements at the end of an array. The corresponding 
+						methods for adding and removing things at the start of an array are called 
+						unshift and shift.
+
+						let todoList = [];
+						function remember(task) {
+							todoList.push(task);
+						}
+						function getTask() {
+							return todoList.shift();
+						}
+						function rememberUrgently(task) {
+							todoList.unshift(task);
+						}
+
+						- To search for a specific value, arrays provide an indexOf method. The method
+						searches through the array from the start to the end and returns the index at
+						which the requested value was found—or -1 if it wasn’t found. To search from
+						the end instead of the start, there’s a similar method called lastIndexOf.
+
+						console.log([1, 2, 3, 2, 1].indexOf(2));
+						// → 1
+						console.log([1, 2, 3, 2, 1].lastIndexOf(2));
+						// → 3
+
+						- Another fundamental array method is slice, which takes start and end indices
+						and returns an array that has only the elements between them. The start
+						index is inclusive, the end index exclusive.
+
+						console.log([0, 1, 2, 3, 4].slice(2, 4));
+						// → [2, 3]
+						console.log([0, 1, 2, 3, 4].slice(2));
+						// → [2, 3, 4]
+
+						- When the end index is not given, slice will take all of the elements after the
+						start index. You can also omit the start index to copy the entire array.
+						- The concat method can be used to glue arrays together to create a new array,
+						similar to what the + operator does for strings.
+
+						function remove(array, index) {
+							return array.slice(0, index)
+							.concat(array.slice(index + 1));
+						}
+						console.log(remove(["a", "b", "c", "d", "e"], 2));
+						// → ["a", "b", "d", "e"]
+
+						- If you pass concat an argument that is not an array, that value will be added
+						to the new array as if it were a one-element array.
+
+String and their properties: 	
+								let kim = "Kim";
+								kim.age = 88;
+								console.log(kim.age);
+								// → undefined
+
+								- Values of type string, number, and Boolean are not objects, 
+								and though the language doesn’t complain if you try to set new 
+								properties on them, it doesn’t actually store those properties.
+
+								- Every string value has a number of methods. Some very useful 
+								ones are slice and indexOf, which resemble the array methods of 
+								the same name.
+
+								console.log("coconuts".slice(4, 7));
+								// → nut
+								console.log("coconut".indexOf("u"));
+								// → 5
+
+								One difference is that a string’s indexOf can search for a string containing
+								more than one character, whereas the corresponding array method looks only
+								for a single element.
+
+								console.log("one two three".indexOf("ee"));
+								// → 11
+
+								- The trim method removes whitespace (spaces, newlines, tabs, 
+								and similar characters) from the start and end of a string.
+
+								console.log(" okay \n ".trim());
+								// → okay
+
+								- padStart takes the desired length and padding character as
+								arguments
+
+								console.log(String(6).padStart(3, "0"));
+								// → 006
+
+								- You can split a string on every occurrence of another string with
+								split and join it again with join.
+
+								let sentence = "Secretarybirds specialize in stomping";
+								let words = sentence.split(" ");
+								console.log(words);
+								// → ["Secretarybirds", "specialize", "in", "stomping"]
+								console.log(words.join(". "));
+								// → Secretarybirds. specialize. in. stomping
+
+								- A string can be repeated with the repeat method, which creates a new 
+								string containing multiple copies of the original string, glued together.
+
+								console.log("LA".repeat(3));
+								// → LALALA
+
+								let string = "abc";
+								console.log(string.length);
+								// → 3
+								console.log(string[1]);
+								// → b
+
+Rest parameters: 	- It can be useful for a function to accept any number of arguments. For example,
+					Math.max computes the maximum of all the arguments it is given.
+					To write such a function, you put three dots before the function’s last parameter,
+					like this:
+
+					function max(...numbers) {
+					let result = -Infinity;
+					for (let number of numbers) {
+					if (number > result) result = number;
+					}
+					return result;
+					}
+					console.log(max(4, 1, 9, -2));
+					// → 9
+
+					- When such a function is called, the rest parameter is bound to an array
+					containing all further arguments. If there are other parameters before it, their
+					values aren’t part of that array. When, as in max, it is the only parameter, it
+					will hold all arguments.
+
+					- You can use a similar three-dot notation to call a function with an array of
+					arguments.
+					let numbers = [5, 1, 7];
+					console.log(max(...numbers));
+					// → 7
+
+					- This “spreads” out the array into the function call, passing its elements as
+					separate arguments. It is possible to include an array like that along with other
+					arguments, as in max(9, ...numbers, 2).
+					Square bracket array notation similarly allows the triple-dot operator to
+					spread another array into the new array.
+
+					let words = ["never", "fully"];
+					console.log(["will", ...words, "understand"]);
+					// → ["will", "never", "fully", "understand"]
+
+The Math object: 	- As we’ve seen, Math is a grab bag of number-related utility functions, such as
+					Math.max (maximum), Math.min (minimum), and Math.sqrt (square root).
+					The Math object is used as a container to group a bunch of related functionality.
+					There is only one Math object, and it is almost never useful as a value.
+					Rather, it provides a namespace so that all these functions and values do not
+					have to be global bindings.
+					- If you need to do trigonometry, Math can help. It
+					contains cos (cosine), sin (sine), and tan (tangent), as well as their inverse
+					functions, acos, asin, and atan, respectively. The number  (pi)—or at least
+					the closest approximation that fits in a JavaScript number—is available as Math
+					.PI. There is an old programming tradition of writing the names of constant
+					values in all caps.
+
+					function randomPointOnCircle(radius) {
+						let angle = Math.random() * 2 * Math.PI;
+						return {x: radius * Math.cos(angle),
+								y: radius * Math.sin(angle)};
+					}
+					console.log(randomPointOnCircle(2));
+					// → {x: 0.3667, y: 1.966}
+
+
+					- The previous example used Math.random. This is a function that returns a
+					new pseudorandom number between zero (inclusive) and one (exclusive) every
+					time you call it.
+
+					console.log(Math.random());
+					// → 0.36993729369714856
+
+					- If we want a whole random number instead of a fractional one, we can use
+					Math.floor (which rounds down to the nearest whole number) on the result of
+					Math.random.
+
+					console.log(Math.floor(Math.random() * 10));
+					// → 2
+
+					- Multiplying the random number by 10 gives us a number greater than or
+					equal to 0 and below 10. Since Math.floor rounds down, this expression will
+					produce, with equal chance, any number from 0 through 9.
+					There are also the functions Math.ceil (for “ceiling”, which rounds up to
+					a whole number), Math.round (to the nearest whole number), and Math.abs,
+					which takes the absolute value of a number, meaning it negates negative values
+					but leaves positive ones as they are.
+
+Destructuring: 	
+				function phi(table) {
+					return (table[3] * table[0] - table[2] * table[1]) /
+					Math.sqrt((table[2] + table[3]) *
+					(table[0] + table[1]) *
+					(table[1] + table[3]) *
+					(table[0] + table[2]));
+				}
+
+				function phi([n00, n01, n10, n11]) {
+					return (n11 * n00 - n10 * n01) /
+					Math.sqrt((n10 + n11) * (n00 + n01) *
+					(n01 + n11) * (n00 + n10));
+				}
+
+				- This also works for bindings created with let, var, or const. If you know the
+				value you are binding is an array, you can use square brackets to “look inside”
+				of the value, binding its contents.
+				A similar trick works for objects, using braces instead of square brackets.
+
+				let {name} = {name: "Faraji", age: 23};
+				console.log(name);
+				// → Faraji
+
+JSON: 	- JavaScript Object Notation - serializes the data, it is converted into a flat description
+		- JSON looks similar to JavaScript’s way of writing arrays and objects, with a
+		few restrictions. All property names have to be surrounded by double quotes,
+		and only simple data expressions are allowed—no function calls, bindings, or
+		anything that involves actual computation. Comments are not allowed in
+		JSON.
+
+		{
+		"squirrel": false,
+		"events": ["work", "touched tree", "pizza", "running"]
+		}
+		
+		- JavaScript gives us the functions JSON.stringify and JSON.parse to convert
+		data to and from this format. The first takes a JavaScript value and returns
+		a JSON-encoded string. The second takes such a string and converts it to the
+		value it encodes.
+
+		let string = JSON.stringify({squirrel: false,
+		events: ["weekend"]});
+		console.log(string);
+		// → {"squirrel":false,"events":["weekend"]}
+		console.log(JSON.parse(string).events);
+		// → ["weekend"]
+
 
 */
 
@@ -254,3 +510,179 @@ const score = {visitors: 0, home: 0};
 score.visitors = 1;
 // This isn't allowed
 score = {visitors: 1, home: 1};
+
+
+
+// Journal of a weresquirell
+//This function checks the correspondence with a formula
+function phi(table) {
+return (table[3] * table[0] - table[2] * table[1]) /
+Math.sqrt((table[2] + table[3]) *
+(table[0] + table[1]) *
+(table[1] + table[3]) *
+(table[0] + table[2]));
+}
+// console.log(phi([76, 9, 4, 1]));
+// // → 0.068599434
+
+// this function does the table for the upstairs formula
+function tableFor(event, journal) {
+	let table = [0, 0, 0, 0];
+	for (let i = 0; i < journal.length; i++) {
+		let entry = journal[i], index = 0;
+		if (entry.events.includes(event)) index += 1;
+		if (entry.squirrel) index += 2;
+		table[index] += 1;
+	}
+	return table;
+}
+// console.log(tableFor("pizza", JOURNAL));
+// → [76, 9, 4, 1]
+
+// for (let entry of JOURNAL) {
+// console.log(`${entry.events.length} events.`);
+// }
+
+
+// Does an array with all the activities
+function journalEvents(journal) {
+	let events = [];
+	for (let entry of journal) {
+		for (let event of entry.events) {
+			if (!events.includes(event)) {
+				events.push(event);
+			}
+		}
+	}
+	return events;
+}
+
+// console.log(journalEvents(JOURNAL));
+// // → ["carrot", "exercise", "weekend", "bread", …]
+
+// for (let event of journalEvents(JOURNAL)) {
+// console.log(event + ":", phi(tableFor(event, JOURNAL)));
+// };
+
+// list only the activities which are occuring more offen
+for (let event of journalEvents(JOURNAL)) {
+	let correlation = phi(tableFor(event, JOURNAL));
+	if (correlation > 0.1 || correlation < -0.1) {
+	console.log(event + ":", correlation);
+	}
+}
+
+// tries out the correspondence between the two most likely results
+for (let entry of JOURNAL) {
+	if (entry.events.includes("peanuts") &&
+		!entry.events.includes("brushed teeth")) {
+		entry.events.push("peanut teeth");
+	}
+}
+
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
+// → 1
+
+
+
+// More arrays!!!!
+let todoList = [];
+function remember(task) {
+	todoList.push(task);
+}
+
+function getTask() {
+	return todoList.shift();
+}
+
+function rememberUrgently(task) {
+	todoList.unshift(task);
+}
+
+console.log([1, 2, 3, 2, 1].indexOf(2));
+// → 1
+console.log([1, 2, 3, 2, 1].lastIndexOf(2));
+// → 3
+
+// Strings
+console.log("coconuts".slice(4, 7));
+// → nut
+console.log("coconut".indexOf("u"));
+// → 5
+
+
+
+console.log("one two three".indexOf("ee"));
+// → 11
+
+
+console.log(" okay \n ".trim());
+// → okay
+
+
+console.log(String(6).padStart(3, "0"));
+// → 006
+
+
+let sentence = "Secretarybirds specialize in stomping";
+let words = sentence.split(" ");
+console.log(words);
+// → ["Secretarybirds", "specialize", "in", "stomping"]
+console.log(words.join(". "));
+// → Secretarybirds. specialize. in. stomping
+
+
+console.log("LA".repeat(3));
+// → LALALA
+
+
+let string = "abc";
+console.log(string.length);
+// → 3
+console.log(string[1]);
+// → b
+
+// rest parameters
+function max(...numbers) {
+	let result = -Infinity;
+	for (let number of numbers) {
+		if (number > result) result = number;
+	}
+	return result;
+}
+
+console.log(max(4, 1, 9, -2));
+// → 9
+
+
+
+let numbers = [5, 1, 7];
+console.log(max(...numbers));
+// → 7
+
+
+// Math object
+function randomPointOnCircle(radius) {
+	let angle = Math.random() * 2 * Math.PI;
+	return {x: radius * Math.cos(angle),
+			y: radius * Math.sin(angle)};
+}
+console.log(randomPointOnCircle(2));
+// → {x: 0.3667, y: 1.966}
+
+
+// Destructuring
+
+function phi(table) {
+	return (table[3] * table[0] - table[2] * table[1]) /
+	Math.sqrt((table[2] + table[3]) *
+	(table[0] + table[1]) *
+	(table[1] + table[3]) *
+	(table[0] + table[2]));
+}
+
+function phi([n00, n01, n10, n11]) {
+	return (n11 * n00 - n10 * n01) /
+	Math.sqrt((n10 + n11) * (n00 + n01) *
+	(n01 + n11) * (n00 + n10));
+}
--- try.js
+++ try.js
@@ -4,25 +4,49 @@
 // for (let event of journalEvents(JOURNAL)) {
 //   let correlation = phi(tableFor(event, JOURNAL));
 //   if (correlation > 0.1 || correlation < -0.1) {
-//     console.log(event + ":", correlation);
-//   }
+/*	
+
+	1. The sum of a range
+
+	The introduction of this book alluded to the following as a nice way to compute
+	the sum of a range of numbers:
+	console.log(sum(range(1, 10)));
+	Write a range function that takes two arguments, start and end, and returns
+	an array containing all the numbers from start up to (and including) end.
+	Next, write a sum function that takes an array of numbers and returns the
+	sum of these numbers. Run the example program and see whether it does
+	indeed return 55.
+	As a bonus assignment, modify your range function to take an optional third
+	argument that indicates the “step” value used when building the array. If no
+	step is given, the elements go up by increments of one, corresponding to the
+	old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7,
+	9]. Make sure it also works with negative step values so that range(5, 2, -1)
+	produces [5, 4, 3, 2].
+
+*/
+
+let range = (start, end, step = 1) => {
+	let rangeArray = [];
+	for (i = start; i <= end; i += step) {
+		rangeArray.push(i);
+	}
+	return rangeArray;
+};
+
+console.log(range(2,7,2))
+
+// console.log(range(5,2,-1));
+
+
+
+// // let numbersArray = [1, 2, 3, 4];
+// let sum = (numbers) => {
+// 	let total = 0;
+// 	for (let entry of numbers) {
+// 		total += entry;
+// 	}
+// 	return total;
 // }
-// → brushed teeth: -0.3805211953
-// → work:          -0.1371988681
-// → reading:        0.1106828054
 
-
-
-let journal = [];
-
-function addEntry(events, squirrel) {
-journal.push({events, squirrel});
-}
-
-
-addEntry(["work", "touched tree", "pizza", "running",
-		"television"], false);
-addEntry(["work", "ice cream", "cauliflower", "lasagna",
-		"touched tree", "brushed teeth"], false);
-addEntry(["weekend", "cycling", "break", "peanuts",
-		"beer"], true);
+// // console.log(sum(numbersArray));
+// console.log(sum(range(1, 10)));
